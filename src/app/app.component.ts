7c634ab1ea0fa0da8ab682e01cab1aec1ebfd830
nsheik15 <nsheik15@users.noreply.github.com>
2020-10-28 17:48:21 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'task-d28-m10-y2020-n01';
+}
